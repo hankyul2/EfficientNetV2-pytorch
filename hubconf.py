e5405f431a9390304b694445424d42f2cf71188d
hankyul <consistant1y@ajou.ac.kr>
2021-11-13 14:07:57 +0900

    feat: Add hubconf.py
    - Add hubconf.py for torchhub

---
+++ hubconf.py
@@ -0,0 +1,29 @@
+from src.efficientnet_v2 import get_efficientnet_v2
+
+
+def efficientnet_v2_s(pretrained=False, **kwargs):
+    return get_efficientnet_v2('efficientnet_v2_s', pretrained, **kwargs)
+
+
+def efficientnet_v2_m(pretrained=False, **kwargs):
+    return get_efficientnet_v2('efficientnet_v2_m', pretrained, **kwargs)
+
+
+def efficientnet_v2_l(pretrained=False, **kwargs):
+    return get_efficientnet_v2('efficientnet_v2_l', pretrained, **kwargs)
+
+
+def efficientnet_v2_s_in21k(pretrained=False, **kwargs):
+    return get_efficientnet_v2('efficientnet_v2_s_in21k', pretrained, **kwargs)
+
+
+def efficientnet_v2_m_in21k(pretrained=False, **kwargs):
+    return get_efficientnet_v2('efficientnet_v2_m_in21k', pretrained, **kwargs)
+
+
+def efficientnet_v2_l_in21k(pretrained=False, **kwargs):
+    return get_efficientnet_v2('efficientnet_v2_l_in21k', pretrained, **kwargs)
+
+
+def efficientnet_v2_xl_in21k(pretrained=False, **kwargs):
+    return get_efficientnet_v2('efficientnet_v2_xl_in21k', pretrained)
